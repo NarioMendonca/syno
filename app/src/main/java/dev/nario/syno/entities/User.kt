ef8318c0b369a952e33bbcce36c5a1a1a35993ac
NarioMendonca <nario.sousa2014@gmail.com>
2026-08-14 11:54:37 -0300

    init project

---
+++ app/src/main/java/dev/nario/syno/entities/User.kt
@@ -0,0 +1,13 @@
+package dev.nario.syno.entities
+
+import java.math.BigDecimal
+
+data class User (
+    val id: String,
+    val name: String,
+    val email: String,
+    val city: String,
+    val latitude: Double,
+    val longitude: Double,
+    val isOnline: Boolean
+)
